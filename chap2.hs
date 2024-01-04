4b5d3543967a5d713f99bba2297c82497611922a
Angel V <angelman@gmail.com>
2024-01-03 19:04:30 -0500

    formatting

--- chap2.hs
+++ chap2.hs
@@ -1,4 +1,6 @@
---Exercise 2.1 The above example is fairly concrete, in that, for one, it is rooted in C major, and furthermore it has a fixed tempo. Define a function twoFiveOne :: Pitch → Dur → Music Pitch such that twoFiveOne p d constructs a ii-V-I chord progression in the key whose major scale begins on the pitch p (i.e., the first degree of the major scale on which the progression is being constructed), where the duration of each of the first two chords is d, and the duration of the last chord is 2 * d. To verify your code, prove by calculation that twoFiveOne (C, 4) wn = t251.
+--Exercise 2.1 The above example is fairly concrete, in that, for one, it is rooted in C major, and furthermore it has a fixed tempo. 
+-- Define a function twoFiveOne :: Pitch → Dur → Music Pitch such that twoFiveOne p d constructs a ii-V-I chord progression in the key whose major scale begins on the pitch p (i.e., the first degree of the major scale on which the progression is being constructed), 
+-- where the duration of each of the first two chords is d, and the duration of the last chord is 2 * d. To verify your code, prove by calculation that twoFiveOne (C, 4) wn = t251.
 import Euterpea
 
 t251 :: Music Pitch
